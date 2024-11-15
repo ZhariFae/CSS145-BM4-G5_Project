f5b1fd755cece2ae827224d9d9aa9d8e1c14b6a9
Zhari <152716710+ZhariFae@users.noreply.github.com>
2024-11-15 13:18:39 +0000

    pred

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ from dataset_information import display_dataset_information
 from eda import display_eda
 from ml_implementation import display_ml_implementation
 from conclusion import display_conclusion
+from prediction import display_prediction
 
 # Streamlit App Configuration
 st.set_page_config(page_title="Mobile Device Usage Analysis", layout="wide")
@@ -12,7 +13,7 @@ st.set_page_config(page_title="Mobile Device Usage Analysis", layout="wide")
 st.sidebar.title("Navigation")
 
 # Main sections as dropdowns
-section = st.sidebar.selectbox("Choose a page:", ["Dataset Information", "Exploratory Data Analysis", "Machine Learning Implementation", "Conclusion"])
+section = st.sidebar.selectbox("Choose a page:", ["Dataset Information", "Exploratory Data Analysis", "Machine Learning Implementation", "Prediction", "Conclusion"])
 
 # Display content based on selected section
 if section == "Dataset Information":
@@ -21,5 +22,5 @@ elif section == "Exploratory Data Analysis":
     display_eda()
 elif section == "Machine Learning Implementation":
     display_ml_implementation()
-elif section == "Conclusion":
-    display_conclusion()
+elif section == "Prediction":
+    display_prediction()
